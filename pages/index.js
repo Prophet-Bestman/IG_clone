ce2649a5f294d7c4601a22ac455523e322247ad6
Prophet-Bestman <bestiproph@gmail.com>
2021-12-24 04:03:48 +0100

    Set up next-auth and firebase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
+  const [id, setId] = useState("");
+  const [secret, setSecret] = useState("");
+
+  // useEffect(() => {
+  //   setId(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
+  //   setSecret(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET);
+
+  //   console.log(`
+  //   ID: ${id}
+  //   Secret: ${secret}
+  //   `);
+  // });
   return (
     <div className="bg-gray-50 min-h-screen">
       <Head>
