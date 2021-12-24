fffc6c1bde46a0a21b7348ece8b9a2de92d6cac9
Prophet-Bestman <bestiproph@gmail.com>
2021-12-24 04:46:58 +0100

    Styled the sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,24 @@ export default function SignIn({ providers }) {
   return (
     <>
       <Header />
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <div className="flex justify-center flex-col items-center h-[85vh]">
+        <img className="w-64" src="/instagram.png" alt="" />
+        <p className="text-lg uppercase text-gray-500 text-center mx-2 my-3">
+          This is not the real app. It is built for portfolio purposes
+        </p>
+        <div className="my-6">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="bg-blue-500 text-white rounded-full py-3 px-5 text-lg font-semibold"
+                onClick={() => signIn(provider.id)}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
